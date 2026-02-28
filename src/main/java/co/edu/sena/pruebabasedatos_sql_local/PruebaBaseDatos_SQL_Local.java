e67b0d205152d90afeb1003b9115449362ba30b7
Jeyson David Zuñiga Gomez <jdzunigag@sena.edu.co>
2026-02-27 19:23:20 -0500

    feat:first commit
    Se suben los archivos bases del proyecto

---
+++ src/main/java/co/edu/sena/pruebabasedatos_sql_local/PruebaBaseDatos_SQL_Local.java
@@ -0,0 +1,9 @@
+package co.edu.sena.pruebabasedatos_sql_local;
+
+
+public class PruebaBaseDatos_SQL_Local {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo...");
+    }
+}
